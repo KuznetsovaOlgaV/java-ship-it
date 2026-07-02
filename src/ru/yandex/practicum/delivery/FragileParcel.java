3f17e5ecf64038b662b562a518be6dac8b8699e8
Olga Kuznetsova <KuznetsovaOlgaV@yandex.ru>
2026-07-02 19:01:28 +0300

    Спринт 5. ТЗ ShipIt

---
+++ src/ru/yandex/practicum/delivery/FragileParcel.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.delivery;
+
+public class FragileParcel extends Parcel implements Trackable {
+    private static final int BASE_COST = 4;
+
+    public FragileParcel(String description, int weight, String deliveryAddress, int sendDay) {
+        super(description, weight, deliveryAddress, sendDay);
+    }
+
+    @Override
+    public void packageItem() {
+        System.out.println("Посылка " + description + " обёрнута в защитную плёнку");
+        printPackaged();
+    }
+
+    @Override
+    public void deliver() {
+        System.out.println("Посылка " + description + " доставлена по адресу " + deliveryAddress);
+    }
+
+    @Override
+    protected int getBaseCostPerKg() {
+        return BASE_COST;
+    }
+
+    @Override
+    public void reportStatus(String newLocation) {
+        System.out.println("Хрупкая посылка "+ description + " изменила местоположение на " + newLocation);
+    }
+}
--- test/ru/yandex/practicum/DeliveryCostTest.java
+++ test/ru/yandex/practicum/DeliveryCostTest.java
@@ -1,4 +1,48 @@
 package ru.yandex.practicum;
 
-public class DeliveryCostTest {
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.delivery.FragileParcel;
+import ru.yandex.practicum.delivery.Parcel;
+import ru.yandex.practicum.delivery.PerishableParcel;
+import ru.yandex.practicum.delivery.StandardParcel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliveryCostTest {
+
+    @Test
+    void standardParcelCost() {
+        Parcel p = new StandardParcel("Книга", 5, "ул. Ленина", 1);
+        assertEquals(10.0, p.calculateDeliveryCost(), 0.001);
+    }
+
+    @Test
+    void fragileParcelCost() {
+        Parcel p = new FragileParcel("Сервис", 3, "ул. Мира", 2);
+        assertEquals(12.0, p.calculateDeliveryCost(), 0.001);
+    }
+
+    @Test
+    void perishableParcelCost() {
+        Parcel p = new PerishableParcel("Тумба", 2, "ул. Деревянная", 3, 2);
+        assertEquals(6.0, p.calculateDeliveryCost(), 0.001);
+    }
+
+    @Test
+    void zeroWeightCost() {
+        Parcel p = new StandardParcel("Письмо", 0, "ул. Невесомая", 1);
+        assertEquals(0.0, p.calculateDeliveryCost(), 0.001);
+    }
+
+    @Test
+    void isExpired_notExpired() {
+        PerishableParcel p = new PerishableParcel("Фрукты", 1, "ул. Фруктовая", 10, 5);
+        assertFalse(p.isExpired(14)); // срок не вышел
+    }
+
+    @Test
+    void isExpired_expired() {
+        PerishableParcel p = new PerishableParcel("Фрукты", 1, "ул. Фруктовая", 10, 5);
+        assertTrue(p.isExpired(16)); // срок вышел
+    }
 }
